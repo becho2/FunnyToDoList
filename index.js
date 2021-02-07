28c18291b59e3c9d4a96104b01acfeb675818b47
chamcham1 <ditivenet@gmail.com>
2021-02-07 16:06:30 +0900

    Test1

---
+++ index.js
@@ -0,0 +1,42 @@
+
+//pls help me
+//to do: finish this
+
+//string
+// const what = "Nicolas";
+// const emoji = "🎨🎨🎨🎨🎨🎨🎨🎨🎨🥾🥾🥾🥾☺☺☺💢💥💥💢💢💥💥💥💥💥💥💢💢💢💢";
+// document.write(emoji);
+// console.log(emoji);
+// console.log(what);
+
+// let a = 221;
+// let b = a - 5;
+// $ = 4
+
+// document.write(b, a);
+// console.log(b);
+
+//Boolean
+// const what = true;
+
+//Number
+// const wat = 666;
+
+//Float
+// const wat = 55.1;
+// console.log(wat);
+
+// const monday = "Mon";
+// const tue = "Tue";
+// const daysOfWeek = ["Mon","Tue","Wed","Thu","Fri", "Sat", "Sun", true];
+// console.log(daysOfWeek[2]);
+
+// console.log(console);
+// console.log(document);
+
+function sayHello(nameOfFriend, age){
+    console.log('Hello!', nameOfFriend,"You have", age, "years old.");
+}
+
+sayHello("Nicole","32");
+
