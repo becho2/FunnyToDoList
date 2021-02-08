468a08840266f204d13e04d9991a439fabd21664
JinisHome <kimjh620@naver.com>
2021-02-09 06:31:00 +0900

    3.3강 끝 근데 똑같이 코딩했는데 강의에선 엔터누르자마자 input 칸이 사라지는데 난 안사라진다? preventDefault때문에 안 사라지게 됐는데 왜 강의영상에서도 같은 코드인데 사라지지?

--- greeting.js
+++ greeting.js
@@ -7,16 +7,34 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
+
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
 function paintGreeting(text){
     greeting.classList.remove(SHOWING_CN);
     greeting.innerText = `Hello ${text}`;
     greeting.classList.add(SHOWING_CN);
 }
 
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+
+}
+
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null){
-
+        askForName();
     } else {
         paintGreeting(currentUser);
     }
