569e68a5e317fcfba64b73590df4caa72877debc
JinisHome <kimjh620@naver.com>
2021-02-11 07:16:12 +0900

    delBtn시 event.target.parentElement를 통해 바로 클릭된 녀석의 부모인 li를 삭제하는 방식으로 나름대로 짜봤던 것을 localstorage 값까지 바꾸기 위해 id를 이용하는 방식으로 바꾸기 전 커밋. ToDolist part two 5분

--- todo.js
+++ todo.js
@@ -6,35 +6,52 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 // 모듈을 분리하면 괜찮은데 그 방법은 유튜브클론 강의에 있다고 함.
 
 const TODOS_LS = 'toDos';
+const toDos = [];
 
-// function saveToDos(text){
-//     localStorage.setItem(TODOS_LS, text);
-// }
+function saveToDos(array){
+ 
+    // toDos = toDos.push(text);
+    // console.log(toDos);
+    localStorage.setItem(TODOS_LS, JSON.stringify(array));
+} 
+//혼자 해보려다 실패
+
+function handleDelBtn(event){
+    const currentToDo_li = event.target.id;
+    currentToDo_li.remove();
+
+}
 
 function paintToDo(text){
     const li = document.createElement("li");
-    const delBtn = document.createElement("button");
-    delBtn.innerHTML = "❌";
+    const delBtn = document.createElement("button");    
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.innerHTML = "❌";
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
-
+    li.id = newId;
     toDoList.appendChild(li);
-    
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos(toDos);
 }
 
 function handleSubmit(event){
     event.preventDefault();
     const currentToDoValue = toDoInput.value;
     paintToDo(currentToDoValue);
-    // saveToDos(currentToDoValue);
+    // saveToDos(currentToDoValue); 
     toDoInput.value = "";
 }
 
 function loadToDos(){
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null){
+    const loadedTodos = localStorage.getItem(TODOS_LS);
+    if(loadedTodos !== null){
         // toDoList.innerText = `${toDos}`;
     }
 
@@ -44,5 +61,7 @@ function loadToDos(){
 function init(){
     loadToDos();
     toDoForm.addEventListener("submit", handleSubmit);
+ 
+    toDoList.addEventListener("click", handleDelBtn);
 }
 init();
