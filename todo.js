ffce28b3e75a0e3a741bae2f2ee2b992d7a30e7d
JinisHome <kimjh620@naver.com>
2021-02-11 11:03:30 +0900

    ToDoList part three 끝 string으로 돼있는 값을 int로 변경, removeChilde, const를 let으로 변경.

--- todo.js
+++ todo.js
@@ -6,18 +6,49 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 // 모듈을 분리하면 괜찮은데 그 방법은 유튜브클론 강의에 있다고 함.
 
 const TODOS_LS = 'toDos';
-const toDos = [];
+let toDos = [];
 
 function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 } 
 //혼자 해보려다 실패
 
-function handleDelBtn(event){
-    const currentToDosId = event.target.id;
-    const currentToDosLi = toDoList.querySelector(`#${currentToDosId}`);
-    // currentToDosLi.remove();
-    // toDos.splice()
+// function handleDelBtn(event){
+//     const currentToDosId = event.target.id;
+//     const currentToDosLi = toDoList.querySelector(`#${currentToDosId}`);
+//     // currentToDosLi.remove();
+//     // toDos.splice()
+// }
+// function filterFn(toDo){
+//     return toDo.id === 1;
+// }
+
+function deleteToDo(event){
+    // console.dir(event);
+    // console.log(event.target.parentNode);
+    const btn = event.target;
+    const li = btn.parentNode;
+    // console.log(toDoList);
+    // console.dir(toDoList);
+    toDoList.removeChild(li);
+    // console.log(li.id);
+    const cleanToDos = toDos.filter(function(toDo){
+
+        return toDo.id !== parseInt(li.id);
+    });
+    // console.log(cleanToDos);
+    // cleanToDos가 의도한대로 클릭된 것을 제외한 Array로 나오는 걸 확인했으니, ToDos를 replace해준다.
+    // 이를 위해 const로 선언했던 toDos를 let으로 바꿔준다
+    toDos = cleanToDos; 
+    // console.log(toDos);
+    saveToDos();
+    // localStorage.setItem(TODOS_LS, JSON.stringify(cleanToDos));
+    // loadToDos();
+ 
+    // const deleteLi = event.target.parentNode;
+    // deleteLi.remove();
+    // const currentToDosId = event.target.id;
+    
 }
 
 function paintToDo(text){
@@ -26,6 +57,7 @@ function paintToDo(text){
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     delBtn.innerHTML = "❌";
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
@@ -61,16 +93,12 @@ function loadToDos(){
         parsedToDos.forEach(function(toDo){
             paintToDo(toDo.text);
         });
-        
     }
-
 }
-
 
 function init(){
     loadToDos();
     toDoForm.addEventListener("submit", handleSubmit);
- 
-    toDoList.addEventListener("click", handleDelBtn);
+    // toDoList.addEventListener("click", handleDelBtn);
 }
 init();
