b187fbaf8a45efcb0a9d5b7f2f3ef1ebebc76943
JinisHome <kimjh620@naver.com>
2021-02-13 08:33:03 +0900

    로컬에 있는 2개의 이미지 파일만 배경으로 이용하도록 우선 변경

--- bg.js
+++ bg.js
@@ -1,40 +1,26 @@
 
-const UNSPLASH_API_KEY = "u6tWjbvOWBGP-qtcuKtMOlRSbJ-_mgw2UhmahMp6nMI";
-const PEXELS_API_KEY = "563492ad6f91700001000001eb1ada34adf94785a7842601fbf05d6d";
+// const UNSPLASH_API_KEY = "u6tWjbvOWBGP-qtcuKtMOlRSbJ-_mgw2UhmahMp6nMI";
 const body = document.querySelector("body");
 
+const IMG_NUMBER = 2;
 
-function getBgImage(lat, lon){
-    //데이터를 얻는 방법은 간단해 fetch를 이용하면 돼 
-    fetch(
-        `https://api.pexels.com/v1`
-    ).then(function (response){
-        console.log(response);
-        return response.json();
-    }).then(function(json){
-        const temperature = json.main.temp;
-        const place = json.name;
-        weather.innerText = `${temperature} @ ${place}`;
-    });
+
+function handleImgLoad(){
+    console.log("finished log")
 }
 
-// function handleImgLoad(){
-//     console.log("finished log")
-// }
-
 function paintImage(imgNumber){
-    const unsplash = unsplash.photos.getPhoto(`UNSPLASH_API_KEY`);
-
-	console.log('unsplash');
+    // const unsplash = unsplash.photos.getPhoto(`UNSPLASH_API_KEY`);
+	// console.log('unsplash');
     const image = new Image();
-    // image.src = `https://source.unsplash.com/category/nature/weekly`;
+    image.src = `https://imgs/${imgNumber}`;
     image.classList.add("bgImage");
     body.appendChild(image);
-    // image.addEventListener("loadend",handleImgLoad)
+    image.addEventListener("loadend",handleImgLoad)
 
-    // body.style.backgroundRepeat = "no-repeat";
-    // body.style.backgroundImage = `url('imgs/${imgNumber + 1}.jpg')`;
-    // body.style.backgroundSize = "100%";
+    body.style.backgroundRepeat = "no-repeat";
+    body.style.backgroundImage = `url('imgs/${imgNumber + 1}.jpg')`;
+    body.style.backgroundSize = "100%";
 }
 
 function genRandom(){
@@ -43,7 +29,8 @@ function genRandom(){
 }
 
 function init(){
-    getBgImage();
+    const randomNumber = genRandom();
+    paintImage(randomNumber);
 }
 
 init();
