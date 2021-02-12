46d4180d5aa7a1b51adc7a109fc96d6e27fea2e8
JinisHome <kimjh620@naver.com>
2021-02-13 08:14:17 +0900

    배경이미지 작업 중 (unsplash random image daily cat -> 세로가 너무 길게 나가는 문제발생

--- bg.js
+++ bg.js
@@ -1,9 +1,22 @@
 
 const UNSPLASH_API_KEY = "u6tWjbvOWBGP-qtcuKtMOlRSbJ-_mgw2UhmahMp6nMI";
+const PEXELS_API_KEY = "563492ad6f91700001000001eb1ada34adf94785a7842601fbf05d6d";
 const body = document.querySelector("body");
 
-const IMG_NUMBER = 3;
 
+function getBgImage(lat, lon){
+    //데이터를 얻는 방법은 간단해 fetch를 이용하면 돼 
+    fetch(
+        `https://api.pexels.com/v1`
+    ).then(function (response){
+        console.log(response);
+        return response.json();
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 // function handleImgLoad(){
 //     console.log("finished log")
@@ -30,8 +43,7 @@ function genRandom(){
 }
 
 function init(){
-    const randomNumber = genRandom();
-    paintImage(randomNumber);
+    getBgImage();
 }
 
 init();
