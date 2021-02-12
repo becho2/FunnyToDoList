c070712a211ff3b36d25e154cc36f8d9c71f78fc
JinisHome <kimjh620@naver.com>
2021-02-13 08:44:15 +0900

    원래 bg.js로 롤백

--- bg.js
+++ bg.js
@@ -4,23 +4,13 @@ const body = document.querySelector("body");
 
 const IMG_NUMBER = 2;
 
-
-function handleImgLoad(){
-    console.log("finished log")
-}
-
 function paintImage(imgNumber){
     // const unsplash = unsplash.photos.getPhoto(`UNSPLASH_API_KEY`);
 	// console.log('unsplash');
     const image = new Image();
-    image.src = `https://imgs/${imgNumber}`;
+    image.src = `imgs/${imgNumber+1}.jpg`;
     image.classList.add("bgImage");
     body.appendChild(image);
-    image.addEventListener("loadend",handleImgLoad)
-
-    body.style.backgroundRepeat = "no-repeat";
-    body.style.backgroundImage = `url('imgs/${imgNumber + 1}.jpg')`;
-    body.style.backgroundSize = "100%";
 }
 
 function genRandom(){
