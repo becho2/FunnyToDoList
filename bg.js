4801120563aeb981512fbaa67ad807f0599c1cac
JinisHome <kimjh620@naver.com>
2021-02-20 12:49:17 +0900

    add manifest.json file

--- bg.js
+++ bg.js
@@ -1,27 +1,29 @@
 
+
+//현재 이용하지 않는 파일
 // const UNSPLASH_API_KEY = "u6tWjbvOWBGP-qtcuKtMOlRSbJ-_mgw2UhmahMp6nMI";
 // const body = document.querySelector("body");
-const container = document.querySelector(".container")
+// const container = document.querySelector(".container")
 
-const IMG_NUMBER = 2;
+// const IMG_NUMBER = 2;
 
-function paintImage(imgNumber){
-    // const unsplash = unsplash.photos.getPhoto(`UNSPLASH_API_KEY`);
-	// console.log('unsplash');
-    const image = new Image();
-    image.src = `imgs/2.jpg`;
-    image.classList.add("bgImage");
-    body.appendChild(image);
-}
+// function paintImage(imgNumber){
+//     // const unsplash = unsplash.photos.getPhoto(`UNSPLASH_API_KEY`);
+// 	// console.log('unsplash');
+//     const image = new Image();
+//     image.src = `imgs/2.jpg`;
+//     image.classList.add("bgImage");
+//     body.appendChild(image);
+// }
 
-function genBgRandom(){
-    const number = Math.ceil(Math.random() * IMG_NUMBER);
-    return number;
-}
+// function genBgRandom(){
+//     const number = Math.ceil(Math.random() * IMG_NUMBER);
+//     return number;
+// }
 
-function init(){
-    const randomNumber = genBgRandom();
-    paintImage(randomNumber);
-}
+// function init(){
+//     const randomNumber = genBgRandom();
+//     paintImage(randomNumber);
+// }
 
-init();
+// init();
